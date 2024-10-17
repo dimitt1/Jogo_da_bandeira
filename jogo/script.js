1952cd7fe8e2d8155a402305bc23464466f3a454
nayara-oliveira <nayara.oliveira@dcomp.ufs.br>
2024-10-17 00:01:58 -0300

    Modifica função do mapa

--- jogo/script.js
+++ jogo/script.js
@@ -32,23 +32,47 @@ const estadosInfo = { // cria registro com estado e capital
 
 // Cria uma função para verificar palpite do mapa 
 const palpiteMapa = (evento) => { 
-const texto = evento.target.id; // pega o id da área clicada
-if(texto == sigla) { // compara o id com a sigla do estado da rodada
-    document.getElementById("txtEstado").textContent = 'Correto!!'
-    document.getElementById("respostaDiv").style.display ='initial'
-    document.getElementById("h5").style.display = 'initial'
-    document.getElementById("s0").disabled = false
-    document.getElementById("mapa").style.display = 'none';
-}
-else {
-    document.getElementById("txtEstado").textContent = 'Incorreto!!'
-}
+    const errados = [...document.getElementsByClassName('errados')] // pegamos os estados já clicados e diferentes da bandeira da rodada
+    const certo = [...document.getElementsByClassName('estadoCerto')]
+    const texto = evento.target.id // pega o id da área clicada
+    if (texto == 'svg') {
+        document.getElementById("txtEstado").textContent = 'Clique sobre o mapa!'
+    }
+    else if (texto == sigla && certo.length != 1) { // compara o id com a sigla do estado da rodada e verifica se já foi criada a classe para o estado certo
+        document.getElementById("txtEstado").textContent = 'Correto!!'
+        document.getElementById("s0").disabled = false 
+        document.getElementById("mapa").style.display = 'none'
+        document.getElementById("respostaDiv").style.display ='initial'
+        document.getElementById("h5").style.display = 'initial'
+        document.getElementById("s0").style.display = 'initial'
+        document.getElementById("s1").style.display = 'initial'
+        document.getElementById("s2").style.display = 'initial'
+        document.getElementById("botaoChute").style.display = 'initial'
+        document.getElementById("botaoProximo").style.display = 'initial'
+    }
+    else {
+        if (errados.length == 2) { // se usuário já errou 2 vezes
+            document.getElementById("txtEstado").textContent = 'Tente Novamente!!'
+            document.getElementById(sigla).classList.add('estadoCerto') // cria classe para estado certo 
+            document.getElementById('botaoProximo').style.display = 'initial'
+            document.getElementById('botaoProximo').disabled = false
+        }
+        else {
+            document.getElementById("txtEstado").textContent = 'Incorreto!!'
+            document.getElementById(texto).classList.add('errados')
+        }
+    }
 }
 
-// Cria uma função para esconder o formulário
+// Cria uma função para esconder o formulário 
+// (agora a função esconde cada elemento individualmente, em vez da div inteira)
 const esconder = () => {
-    document.getElementById("respostaDiv").style.display = 'none';
-    document.getElementById("h5").style.display = 'none';
+    document.getElementById("s0").style.display = 'none'
+    document.getElementById("s1").style.display = 'none'
+    document.getElementById("s2").style.display = 'none'
+    document.getElementById("botaoChute").style.display = 'none'
+    document.getElementById("botaoProximo").style.display = 'none'
+    document.getElementById("h5").style.display = 'none'
     mostrarBandeira()
 }
 //Função para mostrar a bandeira, ao clicar no botão próximo
